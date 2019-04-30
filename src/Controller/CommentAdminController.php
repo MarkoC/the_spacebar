d1dff934258a46ef0ddeab81d1b08b5a66fdbbc8
Marko <marko.curkovic@profil-klett.hr>
2019-04-30 13:49:18 +0200

    now paggination

--- src/Controller/CommentAdminController.php
+++ src/Controller/CommentAdminController.php
@@ -4,15 +4,16 @@ namespace App\Controller;
 use App\Repository\CommentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\HttpFoundation\Request;
 class CommentAdminController extends AbstractController
 {
     /**
      * @Route("/admin/comment", name="comment_admin")
      */
-    public function index(CommentRepository $repository)
+    public function index(CommentRepository $repository, Request $request)
     {
-        $comments = $repository->findBy([], ['createdAt' => 'DESC']);
+        $q = $request->query->get('q');
+        $comments = $repository->findAllWithSearch($q);
         return $this->render('comment_admin/index.html.twig', [
             'comments' => $comments,
         ]);

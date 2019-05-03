bfcc9a7f2e1aea2c389f557d6747dd1d0f5e4dc5
Marko <marko.curkovic@profil-klett.hr>
2019-05-03 09:58:40 +0200

    fixtures

--- src/DataFixtures/CommentFixture.php
+++ src/DataFixtures/CommentFixture.php
@@ -1,31 +1,35 @@
 <?php
 
 namespace App\DataFixtures;
+
 use App\Entity\Article;
 use App\Entity\Comment;
-use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
 
-class CommentFixture extends BaseFixtures implements DependentFixtureInterface
+class CommentFixture extends BaseFixture implements DependentFixtureInterface
 {
     protected function loadData(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
-        $this->createMany(Comment::class, 100, function(Comment $comment, $count) {
+        $this->createMany(100, 'main_comments', function() {
+            $comment = new Comment();
             $comment->setContent(
-                          $this->faker->boolean ? $this->faker->paragraph : $this->faker->sentences(2, true)
+                $this->faker->boolean ? $this->faker->paragraph : $this->faker->sentences(2, true)
             );
+
             $comment->setAuthorName($this->faker->name);
             $comment->setCreatedAt($this->faker->dateTimeBetween('-1 months', '-1 seconds'));
             $comment->setIsDeleted($this->faker->boolean(20));
-            $comment->setArticle($this->getRandomReference(Article::class));
+            $comment->setArticle($this->getRandomReference('main_articles'));
 
+            return $comment;
         });
+
         $manager->flush();
     }
+
     public function getDependencies()
-   {
-       return [ArticleFixtures::class];
-   }
+    {
+        return [ArticleFixtures::class];
+    }
 }
--- src/DataFixtures/TagFixture.php
+++ src/DataFixtures/TagFixture.php
@@ -1,16 +1,21 @@
 <?php
 
 namespace App\DataFixtures;
+
 use App\Entity\Tag;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class TagFixtures extends BaseFixtures
+class TagFixture extends BaseFixture
 {
     protected function loadData(ObjectManager $manager)
     {
-      $this->createMany(Tag::class, 10, function(Tag $tag) {
+        $this->createMany(10, 'main_tags', function() {
+            $tag = new Tag();
             $tag->setName($this->faker->realText(20));
+
+            return $tag;
         });
+
         $manager->flush();
     }
 }
